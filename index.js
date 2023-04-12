8f0e5a0349c76c22fb411344d8150a6cb8efe764
moff <moffmuriithi98@gmail.com>
2023-04-13 01:51:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let left = 0;
+  let right = word.length - 1;
+
+  while (left < right) {
+    if (word[left] !== word[right]) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
